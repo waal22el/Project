4025244b73efd259d2011eb043e2525d64d5b75d
Alma Warin <waal22elstudent.ju.se>
2025-04-13 19:02:06 +0200

    Health bar

---
+++ Assets/EnemyAttack.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public int damage;
+    public PlayerHealth playerHealth;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            playerHealth.TakeDamage(damage);
+        }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int MoveUpwards = 0;
+    public int MoveLeft = 0;
+    public int MoveRight = 0;
+    public int MoveDown = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForceY(MoveUpwards);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.AddForceX(-MoveLeft);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.AddForceX(MoveRight);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            rb.AddForceY(-MoveDown);
+        }
+
+    }
+
+}
