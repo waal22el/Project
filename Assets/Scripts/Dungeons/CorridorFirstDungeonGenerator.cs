cebeb7a5e5856a3dbbbbf42214b10620fa7482e0
Yannick Winkler <yannickwinkler3@gmail.com>
2025-05-05 13:06:45 +0200

    Increased corridor width

--- Assets/Scripts/Dungeons/CorridorFirstDungeonGenerator.cs
+++ Assets/Scripts/Dungeons/CorridorFirstDungeonGenerator.cs
@@ -69,7 +69,7 @@ public class CorridorFirstDungeonGenerator : DungeonGenerator
 
  
 
-        CreateCorridors(floorPositions, potentialRoomPositions);
+        List<List<Vector2Int>> corridors = CreateCorridors(floorPositions, potentialRoomPositions);
 
         HashSet<Vector2Int> roomPositions = CreateRooms(potentialRoomPositions);
 
@@ -79,6 +79,13 @@ public class CorridorFirstDungeonGenerator : DungeonGenerator
  
 
         floorPositions.UnionWith(roomPositions);
+
+        for(int i = 0; i < corridors.Count; i++) 
+        {
+            //corridors[i] = IncreaseCorridorSizeByOne(corridors[i]);
+            corridors[i] = IncreaseCorridorSize3by3(corridors[i]);
+            floorPositions.UnionWith(corridors[i]);
+        }
  
 
         tilemapVisulizer.PaintFloorTiles(floorPositions);
@@ -90,6 +97,68 @@ public class CorridorFirstDungeonGenerator : DungeonGenerator
 
  
 
+    }
+
+    private List<Vector2Int> IncreaseCorridorSize3by3(List<Vector2Int> corridor)
+    {
+        List<Vector2Int> newCorridor = new List<Vector2Int>();
+        for(int i = 1; i < corridor.Count; i++)
+        {
+            for(int x = -1; x < 2; x++)
+            {
+                for(int y = -1; y < 2; y++)
+                {
+                    newCorridor.Add(corridor[i - 1] + new Vector2Int(x, y));
+                }
+            }
+        }
+        return newCorridor;
+    }
+
+    public List<Vector2Int> IncreaseCorridorSizeByOne(List<Vector2Int> corridor)
+    {
+        List<Vector2Int> newCorridor = new List<Vector2Int>();
+        Vector2Int previousDirection = Vector2Int.zero;
+
+        for(int i = 1; i < corridor.Count; i++) 
+        {
+            Vector2Int directionFromCell = corridor[i] - corridor[i - 1];
+            if(previousDirection != Vector2Int.zero && directionFromCell != previousDirection)
+            {
+                //handle corner
+                for(int x = -1; x < 2; x++)
+                {
+                    for(int y = -1; y < 2; y++)
+                    {
+                        newCorridor.Add(corridor[i - 1] + new Vector2Int(x,y));
+                    }
+                }
+                previousDirection = directionFromCell;
+
+            }
+            else
+            {
+                //Add a single cell the direction + 90 degrees
+                Vector2Int newCorridorTileOffset = GetDirection90From(directionFromCell);
+                newCorridor.Add(corridor[i - 1]);
+                newCorridor.Add(corridor[i - 1] + newCorridorTileOffset);
+            }
+
+        }
+        return newCorridor;
+    }
+
+    private Vector2Int GetDirection90From(Vector2Int direction)
+    {
+        if(direction == Vector2Int.up)
+            return Vector2Int.right;
+        if(direction == Vector2Int.right)
+            return Vector2Int.down;
+        if(direction == Vector2Int.down)
+            return Vector2Int.left;
+        if(direction == Vector2Int.left)
+            return Vector2Int.up;
+        return Vector2Int.zero;
     }
 
     private void CreateRoomsAtDeadEnd(List<Vector2Int> deadEnds, HashSet<Vector2Int> roomFloors)
@@ -138,7 +207,7 @@ public class CorridorFirstDungeonGenerator : DungeonGenerator
         return roomPositions;
     }
 
-    private void CreateCorridors(HashSet<Vector2Int> floorPositions,HashSet<Vector2Int> potentialRoomPositions )
+    private List<List<Vector2Int>> CreateCorridors(HashSet<Vector2Int> floorPositions,HashSet<Vector2Int> potentialRoomPositions )
  
 
     {
@@ -147,12 +216,14 @@ public class CorridorFirstDungeonGenerator : DungeonGenerator
         var currentPosition = startPosition;
         potentialRoomPositions.Add(currentPosition);
 
+        List<List<Vector2Int>> corridors = new List<List<Vector2Int>>();        
+
         for (int i = 0; i < corridorCount; i++)
         {
  
 
             var corridor = ProceduralAlgorithms.RandomWalkCorridor(currentPosition, corridorLength);
- 
+            corridors.Add(corridor);
 
             currentPosition = corridor[corridor.Count - 1];
             potentialRoomPositions.Add(currentPosition);
@@ -162,7 +233,7 @@ public class CorridorFirstDungeonGenerator : DungeonGenerator
  
 
         }
- 
+        return corridors;
 
     }
  
