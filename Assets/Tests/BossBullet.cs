143feaa5d081c64e7dbbaf83ada1eb39f3c42880
Yannick Winkler <yannickwinkler3@gmail.com>
2025-05-14 15:49:27 +0200

    idk

---
+++ Assets/Tests/BossBullet.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private int damage = 1;
+    [SerializeField] private float lifetime = 3f;
+    [SerializeField] private GameObject fireObject;
+
+    private GameObject player;
+
+    private Rigidbody2D rb;
+    private float time = 0f;
+
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody2D>();
+
+
+        // Hitta spelaren och skjut mot hens position
+        player = GameObject.FindWithTag("Player");
+        if (player != null)
+        {
+            Vector2 direction = (player.transform.position - transform.position).normalized;
+            rb.linearVelocity = direction * speed;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        time += Time.deltaTime;
+
+        if(time >= lifetime)
+        {
+            Instantiate(fireObject, new Vector3(rb.position.x, rb.position.y,0), Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+            PlayerHealth player = collision.GetComponent<PlayerHealth>();
+            if (player != null)
+            {
+                player.TakeDamage(damage);
+                Debug.Log("Hit!");
+            }
+            Instantiate(fireObject, new Vector3(player.transform.position.x, player.transform.position.y,player.transform.position.z), Quaternion.identity);
+            Destroy(gameObject);
+
+        }
+    }
+}
+
+
