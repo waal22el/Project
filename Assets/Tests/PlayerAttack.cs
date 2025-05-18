6fd7ac30f035c4f0bb1eb9417b7187b6b54e340c
Alma Warin <waal22elstudent.ju.se>
2025-05-18 20:07:13 +0200

    Mobs

--- Assets/Tests/PlayerAttack.cs
+++ Assets/Tests/PlayerAttack.cs
@@ -7,11 +7,18 @@ public class PlayerAttack : MonoBehaviour
 
     void Update()
     {
+
         if (Input.GetMouseButtonDown(0)) // Left-click
         {
-            Vector2 mousePos = playerCamera.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
 
+            Debug.Log("Hello");
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(
+    Input.mousePosition.x,
+    Input.mousePosition.y,
+    -Camera.main.transform.position.z // Needed if camera isn't at z=0
+));
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+            
             if (hit.collider != null)
             {
                 Debug.Log("Hit: " + hit.collider.name);
