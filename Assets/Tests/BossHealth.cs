f52577e30a9965f5fdf04d4a930ff81bdb6b9402
Fadde1 <jafa22my@student.ju.se>
2025-05-19 12:10:49 +0200

    Added unit tests

---
+++ Assets/Tests/BossHealth.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossHealth : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int currentHealth;
+
+    public void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            // Ladda meny-scenen direkt efter bossen dör
+            SceneManager.LoadScene("MenuPostBossFight"); 
+
+            // Förstör bossen 
+            Destroy(gameObject);
+        }
+    }
+}
+
